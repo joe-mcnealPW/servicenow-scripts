830fc75f9e2c49772a4dd8abc77adbe3032a90d3
joe-mcnealPW <104178525+joe-mcnealPW@users.noreply.github.com>
2025-08-05 13:55:54 -0600

    Update colors.js

--- colors.js
+++ colors.js
@@ -32,3 +32,27 @@ setCellStyle: function(column, displayValue) {
     // Return null if no matching color configuration found
     return null;
 }
+
+
+
+setCellStyle: function(column, displayValue) {
+    if(c.data.color_mapped.includes(column)) {
+        console.log(column + " should be mapped.");
+        var map = c.data.tabs[c.state.currentIndex].color_mapping;
+
+        for(var i in map) {
+            if(map[i].field == column) {
+                for(var j in map[i].colors) {
+                    for(var k in map[i].colors[j].options) {
+                        if(map[i].colors[j].options[k] == displayValue) {
+                            return {
+                                background: map[i].colors[j].background,
+                                color: map[i].colors[j].font,
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
